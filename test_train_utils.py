48ff2e7318c801567a2e50789fe9c67b6740b7bd
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-03-02 13:34:31 -0500

    load_fasttext done with unit tests

---
+++ test_train_utils.py
@@ -0,0 +1,26 @@
+import torch
+
+from preprocessor import SpacyPreprocessor, PAD, UNK
+from train_utils import load_fasttext
+
+
+def test_load_fasttext():
+    preprocessor = SpacyPreprocessor([PAD, UNK, "my", "name", "is", "peter"])
+    emb = load_fasttext("test-data/test-fasttext.vec", preprocessor)
+    word_ids, _ = preprocessor.preprocess_tokenized(
+        [
+            ["hi", "there", "what's", "your", "name"],
+            ["my", "name", "is", "peter"],
+        ]
+    )
+    embedded = emb(word_ids)
+    # OOVs
+    assert embedded[0, :4].equal(
+        emb(torch.tensor(preprocessor.unk_id)).unsqueeze(0).expand(4, -1)
+    )
+    # name
+    assert embedded[0, 4].equal(emb(torch.tensor(3)))
+    # my name is peter
+    assert embedded[1, :4].equal(emb(torch.tensor([2, 3, 4, 5])))
+    # pad, should be zero
+    assert embedded[1, 4].equal(torch.zeros(300))
---
+++ train_utils.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+from preprocessor import SpacyPreprocessor
+
+
+def load_fasttext(fname: str, preprocessor: SpacyPreprocessor) -> nn.Embedding:
+    with open(fname, "r") as f:
+        _, emb_dim = map(int, f.readline().split())
+
+        data = {}
+        for line in f:
+            tokens = line.rstrip().split(" ")
+            data[tokens[0]] = map(float, tokens[1:])
+    # embedding for pad is initalized to 0
+    # embeddings for OOVs are randomly initialized from N(0, 1)
+    emb = nn.Embedding(
+        len(preprocessor.word_to_id_dict), emb_dim, padding_idx=preprocessor.pad_id
+    )
+    for word, i in preprocessor.word_to_id_dict.items():
+        if word in data:
+            emb.weight[i] = torch.tensor(list(data[word]))
+    return emb
