8578a70da895e446ece29d159827b35e6a00ac3e
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-03-01 21:09:40 -0500

    SpacyPreprocessor now produces masks too

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,6 @@
-from typing import List
+import torch
+
+from typing import List, Tuple
 from spacy.lang.en import English
 
 PAD = "<pad>"
@@ -22,13 +24,33 @@ class SpacyPreprocessor:
     def tokenize(self, s: str) -> List[str]:
         return [t.text.lower() for t in self.tokenizer(s)]
 
-    def pad(self, unpadded_batch: List[List[int]]) -> List[List[int]]:
+    def pad(self, unpadded_batch: List[List[int]]) -> Tuple[torch.Tensor, torch.Tensor]:
+        # return padded tensor and corresponding mask
         max_len = max(len(word_ids) for word_ids in unpadded_batch)
-        return [
-            word_ids + [0] * (max_len - len(word_ids)) for word_ids in unpadded_batch
-        ]
-
-    def preprocess(self, batch: List[str]) -> List[List[int]]:
-        return self.pad(
-            [[self.word_to_id(token) for token in self.tokenize(s)] for s in batch]
+        return (
+            torch.tensor(
+                [
+                    word_ids + [0] * (max_len - len(word_ids))
+                    for word_ids in unpadded_batch
+                ]
+            ),
+            torch.tensor(
+                [
+                    [1] * len(word_ids) + [0] * (max_len - len(word_ids))
+                    for word_ids in unpadded_batch
+                ]
+            ).float(),
         )
+
+    def preprocess_tokenized(
+        self, tokenized_batch: List[List[str]]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.pad(
+            [
+                [self.word_to_id(token) for token in tokenized]
+                for tokenized in tokenized_batch
+            ]
+        )
+
+    def preprocess(self, batch: List[str]) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.preprocess_tokenized([self.tokenize(s) for s in batch])
--- test_preprocessor.py
+++ test_preprocessor.py
@@ -1,25 +1,31 @@
 import pytest
+import torch
 
 from preprocessor import SpacyPreprocessor
 
 
 @pytest.mark.parametrize(
-    "batch,expected",
+    "batch,expected_preprocessed,expected_mask",
     [
         (
             ["My name is Peter"],
-            [[2, 3, 4, 5]],
+            torch.tensor([[2, 3, 4, 5]]),
+            torch.tensor([[1, 1, 1, 1]]).float(),
         ),
         (
             ["my name is peter"],
-            [[2, 3, 4, 5]],
+            torch.tensor([[2, 3, 4, 5]]),
+            torch.tensor([[1, 1, 1, 1]]).float(),
         ),
         (
             ["My name is Peter", "Is my name David?"],
-            [[2, 3, 4, 5, 0], [4, 2, 3, 1, 1]],
+            torch.tensor([[2, 3, 4, 5, 0], [4, 2, 3, 1, 1]]),
+            torch.tensor([[1, 1, 1, 1, 0], [1, 1, 1, 1, 1]]).float(),
         ),
     ],
 )
-def test_spacy_preprocessor(batch, expected):
+def test_spacy_preprocessor(batch, expected_preprocessed, expected_mask):
     sp = SpacyPreprocessor(["<pad>", "<unk>", "my", "name", "is", "peter"])
-    assert sp.preprocess(batch) == expected
+    preprocessed, mask = sp.preprocess(batch)
+    assert preprocessed.equal(expected_preprocessed)
+    assert mask.equal(expected_mask)
