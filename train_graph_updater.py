2276c718fa65339245b7f86b0448a759556cb8b0
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-03-12 19:36:51 -0500

    do not use kwargs

--- train_graph_updater.py
+++ train_graph_updater.py
@@ -204,7 +204,6 @@ class GraphUpdaterObsGen(pl.LightningModule):
         word_vocab_path: Optional[str] = None,
         node_vocab_path: Optional[str] = None,
         relation_vocab_path: Optional[str] = None,
-        **kwargs,
     ) -> None:
         super().__init__()
         self.save_hyperparameters(
