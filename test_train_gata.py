9d495a5303026bc240afcb6495d8e69115e09cb3
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-03-23 15:13:22 -0400

    GATADoubleDQN initialization and update_target_action_selector()

--- test_train_gata.py
+++ test_train_gata.py
@@ -1,6 +1,13 @@
 import pytest
+import torch.nn as nn
 
-from train_gata import request_infos_for_train, request_infos_for_eval, get_game_dir
+from train_gata import (
+    request_infos_for_train,
+    request_infos_for_eval,
+    get_game_dir,
+    GATADoubleDQN,
+)
+from preprocessor import PAD, UNK, BOS, EOS
 
 
 def test_request_infos_for_train():
@@ -41,3 +48,92 @@ def test_get_game_dir(
         )
         == expected_game_dir
     )
+
+
+def test_gata_double_dqn_default_init():
+    gata_ddqn = GATADoubleDQN()
+
+    # train_env is initialized with the test games
+    assert len(gata_ddqn.train_env.gamefiles) == 2
+    assert gata_ddqn.train_env.request_infos == request_infos_for_train()
+    assert gata_ddqn.train_env.batch_size == gata_ddqn.hparams.game_batch_size
+    assert gata_ddqn.train_env.spec.id == "tw-train-v0"
+
+    # val_env is initialized with the test games
+    assert len(gata_ddqn.val_env.gamefiles) == 2
+    assert gata_ddqn.val_env.request_infos == request_infos_for_eval()
+    assert gata_ddqn.val_env.batch_size == gata_ddqn.hparams.game_batch_size
+    assert gata_ddqn.val_env.spec.id == "tw-val-v0"
+
+    # test_env is initialized with the test games
+    assert len(gata_ddqn.test_env.gamefiles) == 2
+    assert gata_ddqn.test_env.request_infos == request_infos_for_eval()
+    assert gata_ddqn.test_env.batch_size == gata_ddqn.hparams.game_batch_size
+    assert gata_ddqn.test_env.spec.id == "tw-test-v0"
+
+    # default words
+    default_word_vocab = [PAD, UNK, BOS, EOS]
+    assert gata_ddqn.preprocessor.word_vocab == default_word_vocab
+    assert gata_ddqn.graph_updater.word_embeddings[0].weight.size() == (
+        len(default_word_vocab),
+        gata_ddqn.hparams.word_emb_dim,
+    )
+
+    # default node_vocab = ['node']
+    assert gata_ddqn.graph_updater.node_name_word_ids.size() == (
+        len(gata_ddqn.node_vocab),
+        1,
+    )
+    assert gata_ddqn.graph_updater.node_name_mask.size() == (
+        len(gata_ddqn.node_vocab),
+        1,
+    )
+
+    # default relation_vocab = ['relation', 'relation reverse']
+    assert gata_ddqn.graph_updater.rel_name_word_ids.size() == (
+        len(gata_ddqn.relation_vocab),
+        2,
+    )
+    assert gata_ddqn.graph_updater.rel_name_mask.size() == (
+        len(gata_ddqn.relation_vocab),
+        2,
+    )
+
+    # main action selector is train mode
+    assert gata_ddqn.action_selector.training
+
+    # target action selector is in train mode
+    assert gata_ddqn.target_action_selector.training
+    # and frozen
+    for param in gata_ddqn.target_action_selector.parameters():
+        assert param.requires_grad is False
+
+    # main and target action selectors should be initialized to be the same
+    for main, target in zip(
+        gata_ddqn.action_selector.parameters(),
+        gata_ddqn.target_action_selector.parameters(),
+    ):
+        assert main.equal(target)
+
+    # graph updater is in eval mode
+    assert not gata_ddqn.graph_updater.training
+    # and frozen
+    for param in gata_ddqn.graph_updater.parameters():
+        assert param.requires_grad is False
+
+
+def test_gata_double_dqn_update_target_action_selector():
+    gata_ddqn = GATADoubleDQN()
+    # scramble layers in the main action selector and update
+    gata_ddqn.action_selector.node_name_word_ids.fill_(42)
+    gata_ddqn.action_selector.node_embeddings = nn.Embedding(
+        gata_ddqn.num_nodes, gata_ddqn.hparams.node_emb_dim
+    )
+
+    # make sure the weights are the same after updating
+    gata_ddqn.update_target_action_selector()
+    for main, target in zip(
+        gata_ddqn.action_selector.parameters(),
+        gata_ddqn.target_action_selector.parameters(),
+    ):
+        assert main.equal(target)
--- train_gata.py
+++ train_gata.py
@@ -1,11 +1,15 @@
 import os
+import torch.nn as nn
 import pytorch_lightning as pl
+import gym
 
 from typing import Optional
 from textworld import EnvInfos
-from hydra.utils import to_absolute_path
 
 from utils import load_textworld_games
+from layers import WordNodeRelInitMixin
+from action_selector import ActionSelector
+from graph_updater import GraphUpdater
 
 
 def request_infos_for_train() -> EnvInfos:
@@ -44,39 +48,182 @@ def get_game_dir(
     )
 
 
-class GATADoubleDQN(pl.LightningModule):
+class GATADoubleDQN(WordNodeRelInitMixin, pl.LightningModule):
     def __init__(
         self,
-        game_dir: str = "data/rl.0.2",
         difficulty_level: int = 1,
         training_size: int = 1,
         max_episode_steps: int = 100,
-        game_batch_size: int = 5,
+        game_batch_size: int = 25,
+        hidden_dim: int = 8,
+        word_emb_dim: int = 300,
+        node_emb_dim: int = 12,
+        relation_emb_dim: int = 10,
+        text_encoder_num_blocks: int = 1,
+        text_encoder_num_conv_layers: int = 3,
+        text_encoder_kernel_size: int = 5,
+        text_encoder_num_heads: int = 1,
+        graph_encoder_num_cov_layers: int = 4,
+        graph_encoder_num_bases: int = 3,
+        action_scorer_num_heads: int = 1,
+        epsilon_anneal_from: float = 1.0,
+        epsilon_anneal_to: float = 0.1,
+        epsilon_anneal_episodes: float = 20000,
+        word_vocab_path: Optional[str] = None,
+        node_vocab_path: Optional[str] = None,
+        relation_vocab_path: Optional[str] = None,
+        pretrained_graph_updater: Optional[GraphUpdater] = None,
+        train_env: Optional[gym.Env] = None,
+        val_env: Optional[gym.Env] = None,
+        test_env: Optional[gym.Env] = None,
+        **kwargs,
     ) -> None:
         super().__init__()
-        game_dir = to_absolute_path(game_dir)
+        self.save_hyperparameters(
+            "difficulty_level",
+            "training_size",
+            "max_episode_steps",
+            "game_batch_size",
+            "hidden_dim",
+            "word_emb_dim",
+            "node_emb_dim",
+            "relation_emb_dim",
+            "text_encoder_num_blocks",
+            "text_encoder_num_conv_layers",
+            "text_encoder_kernel_size",
+            "text_encoder_num_heads",
+            "graph_encoder_num_cov_layers",
+            "graph_encoder_num_bases",
+            "action_scorer_num_heads",
+            "epsilon_anneal_from",
+            "epsilon_anneal_to",
+            "epsilon_anneal_episodes",
+        )
 
         # load envs
-        self.train_env = load_textworld_games(
-            get_game_dir(
-                game_dir, "train", difficulty_level, training_size=training_size
-            ),
-            "train",
-            request_infos_for_train(),
-            max_episode_steps,
-            game_batch_size,
+        if train_env is None:
+            # load the test rl data
+            self.train_env = load_textworld_games(
+                "test-data/rl_games",
+                "train",
+                request_infos_for_train(),
+                max_episode_steps,
+                game_batch_size,
+            )
+        else:
+            self.train_env = train_env
+        if val_env is None:
+            # load the test rl data
+            self.val_env = load_textworld_games(
+                "test-data/rl_games",
+                "val",
+                request_infos_for_eval(),
+                max_episode_steps,
+                game_batch_size,
+            )
+        else:
+            self.val_env = val_env
+        if test_env is None:
+            # load the test rl data
+            self.test_env = load_textworld_games(
+                "test-data/rl_games",
+                "test",
+                request_infos_for_eval(),
+                max_episode_steps,
+                game_batch_size,
+            )
+        else:
+            self.test_env = test_env
+
+        # initialize word (preprocessor), node and relation stuff
+        (
+            node_name_word_ids,
+            node_name_mask,
+            rel_name_word_ids,
+            rel_name_mask,
+        ) = self.init_word_node_rel(
+            word_vocab_path=word_vocab_path,
+            node_vocab_path=node_vocab_path,
+            relation_vocab_path=relation_vocab_path,
         )
-        self.val_env = load_textworld_games(
-            get_game_dir(game_dir, "valid", difficulty_level),
-            "val",
-            request_infos_for_train(),
-            max_episode_steps,
-            game_batch_size,
+
+        # main action selector
+        self.action_selector = ActionSelector(
+            hidden_dim,
+            self.num_words,
+            word_emb_dim,
+            self.num_nodes,
+            node_emb_dim,
+            self.num_relations,
+            relation_emb_dim,
+            text_encoder_num_blocks,
+            text_encoder_num_conv_layers,
+            text_encoder_kernel_size,
+            text_encoder_num_heads,
+            graph_encoder_num_cov_layers,
+            graph_encoder_num_bases,
+            action_scorer_num_heads,
+            node_name_word_ids,
+            node_name_mask,
+            rel_name_word_ids,
+            rel_name_mask,
         )
-        self.test_env = load_textworld_games(
-            get_game_dir(game_dir, "test", difficulty_level),
-            "test",
-            request_infos_for_train(),
-            max_episode_steps,
-            game_batch_size,
+
+        # target action selector
+        self.target_action_selector = ActionSelector(
+            hidden_dim,
+            self.num_words,
+            word_emb_dim,
+            self.num_nodes,
+            node_emb_dim,
+            self.num_relations,
+            relation_emb_dim,
+            text_encoder_num_blocks,
+            text_encoder_num_conv_layers,
+            text_encoder_kernel_size,
+            text_encoder_num_heads,
+            graph_encoder_num_cov_layers,
+            graph_encoder_num_bases,
+            action_scorer_num_heads,
+            node_name_word_ids,
+            node_name_mask,
+            rel_name_word_ids,
+            rel_name_mask,
         )
+        # we don't train the target action selector
+        for param in self.target_action_selector.parameters():
+            param.requires_grad = False
+        # update the target action selector weights to those of the main action selector
+        self.update_target_action_selector()
+
+        # graph updater
+        if pretrained_graph_updater is None:
+            self.graph_updater = GraphUpdater(
+                hidden_dim,
+                word_emb_dim,
+                self.num_nodes,
+                node_emb_dim,
+                self.num_relations,
+                relation_emb_dim,
+                text_encoder_num_blocks,
+                text_encoder_num_conv_layers,
+                text_encoder_kernel_size,
+                text_encoder_num_heads,
+                graph_encoder_num_cov_layers,
+                graph_encoder_num_bases,
+                nn.Embedding(self.num_words, word_emb_dim),
+                node_name_word_ids,
+                node_name_mask,
+                rel_name_word_ids,
+                rel_name_mask,
+            )
+        else:
+            self.graph_updater = pretrained_graph_updater
+        # we use graph updater only to get the current graph representations
+        self.graph_updater.eval()
+        # we don't want to train the graph updater
+        for param in self.graph_updater.parameters():
+            param.requires_grad = False
+
+    def update_target_action_selector(self) -> None:
+        self.target_action_selector.load_state_dict(self.action_selector.state_dict())
